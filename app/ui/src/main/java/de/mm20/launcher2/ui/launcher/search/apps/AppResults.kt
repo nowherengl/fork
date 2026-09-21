6530dfd17d79cf368faf339ac57673a65a7cc401
nowherengl <nowherengl@gmail.com>
2026-09-21 15:10:31 +0530

    Fix ColumnScope lambda type mismatch for ListResults before header

--- app/ui/src/main/java/de/mm20/launcher2/ui/launcher/search/apps/AppResults.kt
+++ app/ui/src/main/java/de/mm20/launcher2/ui/launcher/search/apps/AppResults.kt
@@ -1,39 +1,20 @@
 package de.mm20.launcher2.ui.launcher.search.apps
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import de.mm20.launcher2.profiles.Profile
 import de.mm20.launcher2.search.Application
-import de.mm20.launcher2.ui.R
 import de.mm20.launcher2.ui.ktx.onTwoFingerTap
 import de.mm20.launcher2.ui.launcher.search.common.grid.GridItem
 import de.mm20.launcher2.ui.launcher.search.common.grid.GridResults
 import de.mm20.launcher2.ui.launcher.search.common.list.ListItem
 import de.mm20.launcher2.ui.launcher.search.common.list.ListResults
-import de.mm20.launcher2.ui.layout.BottomReversed
 import de.mm20.launcher2.ui.locals.LocalGridSettings
 
 fun LazyListScope.AppResults(
@@ -60,7 +41,7 @@ fun LazyListScope.AppResults(
     }
 
     // Tabs permanently hidden
-    val before: (@Composable () -> Unit)? = null
+    val before: (@Composable androidx.compose.foundation.layout.ColumnScope.() -> Unit)? = null
 
     val triggerProfileToggle: (androidx.compose.ui.hapticfeedback.HapticFeedback) -> Unit = { haptic ->
         if (profiles.size > 1) {
